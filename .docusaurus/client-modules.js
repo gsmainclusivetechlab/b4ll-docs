0abaa96ccc8d7f192b860506d6bbbdba378a57c1
@vivek.cherian <vivek.cherian@fingent.com>
2021-03-11 23:43:43 +0530

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/fingent/Desktop/WorkSpace/B4 client/b4ll-docs/src/css/custom.css"),
+];

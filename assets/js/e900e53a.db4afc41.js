600ab291e59b128a36202b3afc6908f374e93e74
Eudis Anjos <eudisley@gmail.com>
2021-05-18 13:48:50 +0100

    Deploy website - based on 5cf1789a95a39fdd2f63a3a49649f3d9813d6b25

---
+++ assets/js/e900e53a.db4afc41.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{113:function(o){o.exports=JSON.parse('{"allTagsPath":"/b4ll-docs/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/b4ll-docs/blog/tags/hola"}')}}]);
---
+++ assets/js/ea32765d.37cf4f53.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{114:function(l){l.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/b4ll-docs/blog/welcome"},{"title":"Hello","permalink":"/b4ll-docs/blog/hello-world"},{"title":"Hola","permalink":"/b4ll-docs/blog/hola"}]}')}}]);

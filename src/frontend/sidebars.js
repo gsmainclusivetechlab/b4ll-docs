3e9bf2f418a25352056bb4fa0d8c50f79164820b
Samy <sbouchlaghem@gsma.com>
2021-06-16 14:24:27 +0100

    remove intro page

--- src/frontend/sidebars.js
+++ src/frontend/sidebars.js
@@ -17,7 +17,6 @@ module.exports = {
             'voice/alerts',
         ],
         'Application': [
-            'app/info',
             'app/setup',
             'app/usecases',
             'app/settings'
